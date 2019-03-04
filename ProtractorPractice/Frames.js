efcf7f37070d9013a0e520d4d42d11f2f1d4cd98
atul <atulvij53@gmail.com>
2019-03-04 17:58:36 +0530

    linux code 5th march

---
+++ ProtractorPractice/Frames.js
@@ -0,0 +1,19 @@
+describe("Alerts Practice", function(){
+	it("Opening the site", function(){
+		
+		browser.waitForAngularEnabled(false);
+		
+		browser.driver.manage().window().maximize();
+		browser.get("http://www.qaclickacademy.com/practice.php");
+		
+		//Switch to frame.
+		browser.switchTo().frame("courses-iframe");
+		element(by.css("a[href*='sign_in']")).getText().then(function(text){
+			
+			console.log(text);
+		})
+		
+		
+		
+	})
+})
--- ProtractorPractice/configuration.js
+++ ProtractorPractice/configuration.js
@@ -1,6 +1,6 @@
 exports.config = {
 //seleniumAddress : 'http://localhost:4444/wd/hub',
-specs: ['Alerts.js'],
+specs: ['Frames.js'],
 
 //capabilities: {
 //	  'browserName': 'firefox'
