93f66c2b3916db47538004c41544aec7cfb544f5
atulvij1703 <atulvij53@gmail.com>
2019-03-07 08:20:35 +0530

    windows 7th march

--- ProtractorPractice/PracticeExercise.js
+++ ProtractorPractice/PracticeExercise.js
@@ -1,22 +1,71 @@
-describe("Protractor Commerce", function(){
-	it("Opening the site", function(){
-		
-		browser.get("https://qaclickacademy.github.io/protocommerce/");
-		
-		element(by.name("name")).sendKeys("Test");
-		element(by.name("email")).sendKeys("test@test.test");
-		element(by.id("exampleInputPassword1")).sendKeys("testing");
-		element(by.css("input[type='checkbox']")).click();
-		element(by.cssContainingText("select[class='form-control'] option","Female")).click();
-		element.all(by.className("form-check-label")).get(3).click();
-		element(by.buttonText("Submit")).click();
-		element(by.name("name")).clear();
-		element(by.name("name")).sendKeys("T");
-		element(by.className("alert alert-danger")).getText().then(function(text){
-			
-			console.log(text);
-		
-		})
+describe('Practice Exercise',function() {  
+
+function selectItems(product)
+{
+//take 4 cards into list
+//go through each index in the list - and get the title= if title =desired title then in that index i will select add button
+element.all(by.tagName("app-card")).each(function(item)
+{
+
+item.element(by.css("h4 a")).getText().then(function(text)
+{
+if(text==product)
+{
+item.element(By.css("button[class*='btn-info']")).click();
+}
+}) }) 
+}
+
+it('practice',function() {
 	
+	browser.driver.manage().window().maximize();
+
+
+browser.get("https://qaclickacademy.github.io/protocommerce/");
+
+element(by.name("name")).sendKeys("Mystudents");
+element(by.css("input[name='email']")).sendKeys("mystudents@gmail.com");
+element(by.id("exampleInputPassword1")).sendKeys("password2");
+element(by.css("input[type='checkbox']")).click();
+element(by.cssContainingText("[id='exampleFormControlSelect1'] option","Female")).click();
+element.all(by.name("inlineRadioOptions")).first().click();
+element(by.buttonText("Submit")).click().then(function()
+{
+element(by.css("div[class*='success']")).getText().then(function(text)
+{
+console.log(text);
+})
+})
+element(by.name("name")).clear();
+element(by.name("name")).sendKeys("M").then(function()
+{
+element(by.css("[class='alert alert-danger']")).getText().then(function(text)
+{
+console.log(text);
+})
+})
+element(by.linkText("Shop")).click();
+selectItems("Samsung Note 8");
+selectItems("iphone X");
+
+element(by.partialLinkText("Checkout")).getText().then(function(text)
+{
+var res=text.split("(");
+var x=Number(res[1].trim().charAt(0));
+
+expect(x).toBe(2);
+})
+
+element(by.partialLinkText("Checkout")).click();
+
+element.all(by.css("tbody tr")).each(function(item){
+	
+	item.element(by.css("td:nth-child(4)")).getText().then(function(text){
+		
+		console.log(text);
 	})
+})
+
+})
+
 })
