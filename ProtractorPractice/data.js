29763f0fafb820b9191a2d184173151dd45eb273
atulvij1703 <atulvij53@gmail.com>
2019-03-20 07:51:44 +0530

    20th march windows

---
+++ ProtractorPractice/data.js
@@ -0,0 +1,25 @@
+module.exports={
+datadrive:
+{
+FirstData:
+{
+		firstinput:"2",
+		secondinput:"3",
+		result:"5"
+},
+
+SecondData:
+{
+		firstinput:"4",
+		secondinput:"5",
+		result:"9"
+},
+
+ThirdData:
+{
+		firstinput:"7",
+		secondinput:"3",
+		result:"10"
+}
+}
+}
--- ProtractorPractice/spec1.js
+++ ProtractorPractice/spec1.js
@@ -1,34 +1,44 @@
 describe("Protractor practice",function(){
 	var obj = require("./jsObject.js");
+	var d = require("./data.js");
+	var using = require("jasmine-data-provider");
+	
 	
 	beforeEach(function(){
 		obj.getURL();
 		
 	})
-	it("Opening the site",function(){
+	//data stores the actual data like firstinput etc
+	//description stores the sub object like FirstData etc.
+	//for every iteration one data set is picked.
+	using(d.datadrive, function (data, description) {
+		it("Opening the site",function(){
+			
+			obj.first.sendKeys(data.firstinput);
+			obj.second.sendKeys(data.secondinput);
+			obj.go.click();
+			
+			obj.result.getText().then(function(ans){
 				
-		obj.first.sendKeys("3");
-		obj.second.sendKeys("4");
-		obj.go.click();
-		
-		obj.result.getText().then(function(ans){
-			
-			console.log(ans);
-			
-			if(ans == '7')
-				{
-				console.log("Pass");
-				}
-			else
-				{
-				console.log("Fail");
-				}
+				console.log(ans);
+				
+				if(ans == '7')
+					{
+					console.log("Pass");
+					}
+				else
+					{
+					console.log("Fail");
+					}
 
-		})
-		//Real output will display only when you resolve promise.
+			})
+			//Real output will display only when you resolve promise.
+			
+			expect(obj.result.getText()).toBe(data.result);
+			//Jasmine takes care of promise resolve.
 		
-		expect(obj.result.getText()).toBe("7");
-		//Jasmine takes care of promise resolve.
+	});
+	
 		
 		
 	})	
